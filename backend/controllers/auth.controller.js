bbfa83f767714ceecbe6bb93f64dc2342abef926
Tamtai <107045052+Tainyz@users.noreply.github.com>
2025-05-09 20:23:34 +0700

    connect 2 mongoose

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = (req, res) => {
+    res.json({
+        data : 'Signup Endpoint',
+    });
+}
+
+
+export const login = (req, res) => {
+    res.json({
+        data : 'Login Endpoint',
+    });
+}
+
+export const logout =  (req, res) => {
+    res.json({
+        data : 'Logout Endpoint',
+    });
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import expresss from 'express';
+import { login, logout, signup } from '../controllers/auth.controller.js';
 
 const router = expresss.Router();
 
-router.get('/signup', (req, res) => {
-    res.json({
-        data : 'Signup Endpoint',
-    });
-});
+router.post('/signup',signup );
+
+router.post('/login', login);
+
+router.post('/logout', logout);
 
 export default router;
