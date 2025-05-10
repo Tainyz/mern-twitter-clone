3029e4f4f6474c31711254c96eb44490a19664a7
Tamtai <107045052+Tainyz@users.noreply.github.com>
2025-05-10 16:28:35 +0700

    api done

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import  express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-//2.49.00
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse JSON request body
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded request body
 
-app.use("/api/auth",authRoutes);
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
